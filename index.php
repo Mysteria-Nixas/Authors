0c843b66aa0434e6635d567f397427be3df76f42
Mysteria-Nixas <46866881+Mysteria-Nixas@users.noreply.github.com>
2019-09-10 14:58:29 -0400

    Trying to change to php
    Signed-off-by: Mysteria-Nixas <46866881+Mysteria-Nixas@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Authors</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php includes_once("includes/css.php");?>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="container">
+            <span class="heading">
+                <h1>Favorite Authors</h1>
+            </span>
+            <div>
+                <?php include_once("includes/navBar.php");?>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="card mb-3">
+                        <img src="https://fedora.digitalcommonwealth.org/fedora/objects/commonwealth-oai:mc87r128v/datastreams/thumbnail300/content"
+                            class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h6 class="card-title">Washington Irving</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card mb-3">
+                        <img src="https://mediadc.brightspotcdn.com/dims4/default/a4ff469/2147483647/strip/true/crop/939x939+0+0/resize/939x939!/quality/90/?url=https%3A%2F%2Fmediadc.brightspotcdn.com%2Fad%2F80%2F60108b377c6023f12f30c5733174%2F274d827b45bbe36cf5e3ad654889a512.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h6 class="card-title">Edgar Allan poe</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card mb-3">
+                        <img src="https://www.williamshakespeare.net/images/shakespeare.jpg" class="authorImage" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h6 class="card-title">William Shakespeare</h6>
+                        </div>
+                    </div>
+                </div>
+            <hr />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis augue lectus, luctus in ullamcorper
+                eu,
+                cursus non
+                ligula. Sed vel dignissim ipsum. Proin a nisi velit. Duis eget mauris quis ligula egestas
+                molestie. In
+                eleifend
+                porttitor sapien, luctus convallis eros posuere blandit. Morbi et lacus a sapien tempus
+                tincidunt.
+                Maecenas
+                condimentum, nisi ut efficitur maximus, nibh arcu tincidunt metus, at cursus mauris eros sit
+                amet nisi.
+                Vivamus
+                commodo at quam vel venenatis. Vivamus arcu mauris, tincidunt eget pretium a, egestas vel
+                turpis. Proin
+                feugiat
+                ultrices neque, eget porta erat porta ut. Proin semper rutrum felis, ac commodo risus porttitor
+                at.
+                Phasellus
+                convallis imperdiet elementum. Fusce iaculis tincidunt quam a molestie. Nulla pharetra quis ex
+                vel
+                ultrices.</p>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <footer>
+            <address>
+                123 Death Row<br />
+                Sleepy Hollow<br />
+                Transylvania<br />
+            </address>
+            &copy; 2019, Scary Weird
+        </footer>
+    </div>
+</body>
+
+</html>
